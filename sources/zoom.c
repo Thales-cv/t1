b2d1b422febc77260b8cc1200043c91768b121a5
Thales Veiga <thales.cveiga@gmail.com>
2023-04-26 11:14:08 -0300

    tentativa2

--- sources/zoom.c
+++ sources/zoom.c
@@ -5,13 +5,99 @@ int main(void)
 {
 	struct image_s data;
 	struct image_s *image = &data;
-	int i, j, r, b, g;
+	// ========
+	struct image_s data2;
+	struct image_s *zoomImage = &data2;
 	
-	r = read_ppm("lena.ppm", image);
+	struct pixel_s pixelTarget;
+	
+	int i, j, r, b, g, f, lena;
+	
+	int newHeight;
+	int newWidth;
+
+	// =====
+
+	struct subpixel_s {
+    	struct pixel_s pixels[3][3];
+	};
+
+	/*
+		funcao criaSubPixel(pixel){
+			bytes r,g,b
+			
+
+
+			return matriz[3][3];
+		}
+	*/
+
+
+	lena = read_ppm("lena.ppm", image);
+	newHeight = (image->height) * 3;
+	newWidth = (image->width) * 3;
+	
+	f = new_ppm(zoomImage,newWidth,newHeight);
+
+
+	struct subpixel_s teste(struct pixel_s pixel){
+		struct subpixel_s matrix;
+
+		int r, g, b;
+		r = pixel.r;
+		g = pixel.g;
+		b = pixel.b;
+		printf("R:%d G:%d B:%d",r,g,b);
+    	
+		for(int larg = 0; alt <=2 ; 0++){
+			for(int alt = 0; larg <=2; 0++){
+				if(larg == 0){
+					matrix[alt][larg].r = r;
+					matrix[alt][larg].g = 0;
+					matrix[alt][larg].b = 0;
+				}
+				if(larg == 1){
+					matrix[alt][larg].r = 0;
+					matrix[alt][larg].g = g;
+					matrix[alt][larg].b = 0; 
+				}
+				if(larg == 2){
+					matrix[alt][larg].r = 0;
+					matrix[alt][larg].g = 0;
+					matrix[alt][larg].b = b; 
+				}
+			}
+		}
+		return matrix;
+	}
+
+
+
+	if(f == 0){
+		for (j = 0; j < image->height; j++) {
+			for (i = 0; i < image->width; i++) {
+				
+				/*
+				pixelTarget =  image->pix[j * image->width + i];
+				printf("Pixel Position [%d][%d] -> R:%d G: %d B:%d \n",j,i,
+				pixelTarget.r,
+				pixelTarget.g,
+				pixelTarget.b);
+				*/
+
+				pixelTarget = image->pix[j * image->width + i];
+				struct subpixel_s subTeste = teste(pixelTarget);
+
+
+			}
+		}
+	}
+
 	/*
 	if (r == 0) {
 		
 		printf("width: %d, height: %d\n", image->width, image->height);
+		
 		for (j = 0; j < image->height; j++) {
 			for (i = 0; i < image->width; i++) {
 				printf("(%d %d) %02x %02x %02x\n", i, j,
@@ -25,6 +111,7 @@ int main(void)
 		
 		free_ppm(image);
 	}
+
 	*/
 	///// CÓDIGO PRODUZIDO
 	
@@ -41,19 +128,6 @@ int main(void)
 		}
 	}
 	*/
-
-	pixel1
-	pixel2
-	pixel3
-	pixel4
-
-	lista de pixels
-	newMatrix(lista de pixel);
-	return matrix nova
-
-
-
-
 
 	r = new_ppm(image, 675, 428);
 	
